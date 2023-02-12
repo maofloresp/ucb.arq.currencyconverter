9c1b09af4f125123f574b343e9992e2359dae85f
Mauricio Flores Pauletti <Mauricio.Flores@jala-foundation.org>
2023-02-11 23:08:00 -0400

    .net and kotlin solutions added

---
+++ kotlin/src/test/kotlin/ucb/softarch/currencyconveter/currencyconverter/CurrencyconverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package ucb.softarch.currencyconveter.currencyconverter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyconverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
