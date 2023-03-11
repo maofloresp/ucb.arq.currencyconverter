ec13300346a826ce81eda8f4a2b3088f0b0bf6bd
Mauricio Flores Pauletti <mauricio.flores.p@ucb.edu.bo>
2023-03-11 10:58:05 -0400

    Added form and table to webapp

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+interface Conversion {
+  date: string;
+  from: string;
+  to: string;
+  amount: number;
+  value: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Currency Converter';
+  author = 'Mauricio Flores';
+  currencyForm : FormGroup;
+
+  displayedColumns: string[] = ['date', 'from', 'to', 'amount', 'value'];
+  dataSource =  [
+    {date: '2023-03-11', from: 'EUR', to: 'USD', amount: 778.03, value: 853.12},
+  ];
+  /**
+   *
+   */
+  constructor(private formBuilder: FormBuilder)
+  {
+    this.currencyForm = this.formBuilder.group({
+      from: '',
+      to: '',
+      amount: ''
+    })
+    
+  }
+
+  submit()
+  {
+    console.log(this.currencyForm.value)
+  }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
